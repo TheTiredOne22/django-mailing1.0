09f840f75b429a014121f812ba955b24f33ecf06
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-02-11 15:41:08 +0100

    Mainly setup mailing functionality, update settings.py file

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'mail',
+    'django-mailing.mail',
     'allauth',
     'allauth.account',
     'compressor',
