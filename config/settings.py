fe1b7d27232796e873670f55811ea7160f29ac39
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-02-12 07:33:21 +0100

    Random

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'mail.apps.MailConfig',
+    'mail',
     'allauth',
     'allauth.account',
     'compressor',
